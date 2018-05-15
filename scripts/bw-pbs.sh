e4fcf9b5f673f84f1b197f98cab160e5b7bb4abe
Kristof Farkas-Pall <kristofarkas@gmail.com>
2018-05-15 15:32:25 +0200

    Update mongodb

--- scripts/bw-pbs.sh
+++ scripts/bw-pbs.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-    echo "Number of arguments incorrect! Pass system rmq-port only!"; exit;
-fi
-
-export RADICAL_PILOT_DBURL='mongodb://user:user@ds239009.mlab.com:39009/inspire-2'
+export RADICAL_PILOT_DBURL='mongodb://user:user@ds223760.mlab.com:23760/adaptivity'
 export RADICAL_PILOT_PROFILE=True
 export RADICAL_ENMD_PROFILE=True
 export RADICAL_ENMD_PROFILING=1
@@ -14,4 +10,4 @@ export RADICAL_ENTK_VERBOSE='DEBUG'
 export RADICAL_SAGA_VERBOSE='DEBUG'
 export RADICAL_PILOT_VERBOSE='DEBUG'
 
-python rfe.py $1 $2 &> rfe.rp.session.log
+python rfe.py  &> rfe.rp.session.log
